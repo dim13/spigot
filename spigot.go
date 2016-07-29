61fdd15bd7c563e6251fd757a028cb40e5a846de
Dimitri Sokolyuk <demon@dim13.org>
2016-07-30 00:31:07 +0200

    Comment

--- spigot.go
+++ spigot.go
@@ -1,29 +1,32 @@
 package spigot
 
 /*
-   1. Initialize: Let A = (2, 2, 2, 2,... ,2) be an array of length [10n/3]+1.
+   1. Initialize: Let A = (2, 2, 2, 2,... ,2) be an array of length
+      [10n/3]+1.
 
    2. Repeat n times:
 
       Multiply by 10: Multiply each entry of A by 10.
 
-      Put A into regular form: Starting from the right, reduce the ith element
-      of A (corresponding to b-entry (i - 1)/(2i - 1)) modulo 2i - 1, to get
-      a quotient q and a remainder r. Leave r in place and carry q(i - 1)
-      one place left. The last integer carried (from the position where
-      i - 1 = 2) may be as large as 19.
+      Put A into regular form: Starting from the right, reduce the
+      ith element of A (corresponding to b-entry (i - 1)/(2i - 1))
+      modulo 2i - 1, to get a quotient q and a remainder r. Leave
+      r in place and carry q(i - 1) one place left. The last integer
+      carried (from the position where i - 1 = 2) may be as large
+      as 19.
 
-   3. Get the next predigit: Reduce the leftmost entry of A (which is
-      at most 109 (= 9 - 10 + 191)) modulo 10. The quotient, q, is the new
-      predigit of π, the remainder staying in place.
+   3. Get the next predigit: Reduce the leftmost entry of A (which
+      is at most 109 (= 9 - 10 + 191)) modulo 10. The quotient, q,
+      is the new predigit of π, the remainder staying in place.
 
-   4. Adjust the predigits: If q is neither 9 nor 10, release the held
-      predigits as true digits of π and hold q. If q is 9, add q to the
-      queue of held predigits. If q is 10 then:
+   4. Adjust the predigits: If q is neither 9 nor 10, release the
+      held predigits as true digits of π and hold q. If q is 9, add
+      q to the queue of held predigits. If q is 10 then:
 
       - set the current predigit to 0 and hold it;
       - increase all other held predigits by 1(9 becomes 0);
       - release as true digits of π all but the current held predigit.
+
 */
 
 func Pi(n int) <-chan int {
@@ -79,6 +82,21 @@ func predigit(in <-chan int) <-chan int {
 	}()
 	return c
 }
+
+/*
+   1. Initialize: Let the ﬁrst digit be 2 and initialize an array
+      A of length n + 1 to (1, 1, 1, . . . , 1).
+
+   2. Repeat n − 1 times:
+
+      Multiply by 10: Multiply each entry of A by 10.
+
+      Take the fractional part: Starting from the right, reduce the
+      ith entry of A modulo i + 1, carrying the quotient one place
+      left.
+
+      Output the next digit: The ﬁnal quotient is the next digit of e.
+*/
 
 func E(n int) <-chan int {
 	c := make(chan int)
