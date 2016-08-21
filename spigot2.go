49db2c6077289077ab780c0ba6c986b9b321b90f
Dimitri Sokolyuk <demon@dim13.org>
2016-08-21 20:38:27 +0200

    Simplify

--- spigot2.go
+++ spigot2.go
@@ -27,19 +27,17 @@ func zero(n int) <-chan int {
 // Pi2 calculates n digits of Pi concurently
 func Pi2(n int) <-chan int {
 	c := zero(n + 1)
-	cr := c
 	for i := 10*n/3 + 1; i > 0; i-- {
-		cr = spigot(2, i, 2*i+1, cr)
+		c = spigot(2, i, 2*i+1, c)
 	}
-	return predigit(spigot(2, 1, 10, cr))
+	return predigit(spigot(2, 1, 10, c))
 }
 
 // E2 calculates n digits of E concurently
 func E2(n int) <-chan int {
 	c := zero(n + 1)
-	cr := c
 	for i := n + 1; i > 0; i-- {
-		cr = spigot(1, 1, i+1, cr)
+		c = spigot(1, 1, i+1, c)
 	}
-	return spigot(2, 1, 10, cr)
+	return spigot(2, 1, 10, c)
 }
